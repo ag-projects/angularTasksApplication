f449cbe3f4de16d4c80299d57aa6169be54b5647
Armen G <gharibi.armen@yahoo.com>
2018-01-20 18:27:28 -0800

    updated the task list component

--- src/main/forntend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/forntend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+    taskService: any;
+
+    tasks: Task[] = [];
 
   constructor() { }
 
   ngOnInit() {
+      this.tasks.push( new Task(1, "Task 1", true, "01/20/18") );
+      this.tasks.push( new Task(2, "Task 2", true, "01/22/18") );
+      this.tasks.push( new Task(3, "Task 3", false, "01/23/18") );
   }
+
+    getDueDateLabel(task: Task) {
+      return task.completed ? 'label-success' : 'label-primary';
+    }
+
+    onTaskChange(event, task) {
+      // this.taskService.saveTask(task, event.target.checked).subscribe();
+        console.log('Task has changed!');
+    }
 
 }
